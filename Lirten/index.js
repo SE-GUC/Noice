841fa99768da4b5da9d033629e67400f322c6d39
msharaf315 <47497300+msharaf315@users.noreply.github.com>
2019-03-02 22:33:16 +0300

    Update index.js

--- Lirten/index.js
+++ Lirten/index.js
@@ -5,14 +5,15 @@ const location = require('./routes/api/Location')
 const calender = require('./routes/api/calender')
 const members=require('./routes/api/member')
 const partners = require('./routes/api/partners')
-
+const admins = require ('./routes/api/admins')
 
 const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome to Lirten Hub</h1>
-    `);
+    <a href="/api/admins">Admins</a>
+`);
 })
 
 // Direct routes to appropriate files 
@@ -21,6 +22,7 @@ app.use('/api/member',members)
 app.use('/api/Location', location)
 app.use('/api/calender', calender)
 app.use('/api/partners', partners)
+app.use('/api/admins',admins)
 
 // Handling 404
 app.use((req, res) => {
