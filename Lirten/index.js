7f91a5025aa2559f104674d8f52bcc6ee984491c
kareemelshafey <kareemkimo39@gmail.com>
2019-03-02 12:28:50 +0200

    Update index.js

--- Lirten/index.js
+++ Lirten/index.js
@@ -1,21 +1,18 @@
 const express = require('express')
 
-const users = require('./routes/api/users')
-const books = require('./routes/api/')
+const location = require('./routes/api/Location')
 
 const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Welcome to Book Store</h1>
-    <a href="/api/users">Users</a>
-    <a href="/api/books">Books</a>
+    res.send(`<h1>Welcome to Lirten Hub</h1>
+    <a href="/api/Location">Location</a>
     `);
 })
 
 // Direct routes to appropriate files 
-app.use('/api/users', users)
-app.use('/api/books', books)
+app.use('/api/Location', location)
 
 // Handling 404
 app.use((req, res) => {
