4b431c6e11054c6ba766ae7f1a4d6d4067bf540b
ANDREW-21-1 <andrew.ashraf@live.com>
2019-03-31 21:16:31 +0200

    member model

--- Lirten/routes/api/member.js
+++ Lirten/routes/api/member.js
@@ -5,19 +5,15 @@ const member_controller = require('../../controllers/memberController')
 
 router.get('/',member_controller.getAllMember)
 
-// Create a Partner
 router.post('/',member_controller.createMember)
 
-// Update a Partner
+
 router.put('/:id', member_controller.updateMember)
 
-// Delete a Partner
+
 router.delete('/:id',member_controller.deleteMember)
 
-//Find a Partner by ID
+
 router.get('/:id',member_controller.findMember)
 
-
-
 module.exports = router
-
