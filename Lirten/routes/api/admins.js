7df42d0318e984f30861a91dda3825ae2562c423
msharaf315 <msharaf315@gmail.com>
2019-03-31 23:56:09 +0300

    added the other entites controllers to admin api and admin controllers

--- Lirten/routes/api/admins.js
+++ Lirten/routes/api/admins.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+
 const Admin = require('../../models/Admin')
 const validator = require('../../validations/adminValidations')
-//require controllers
-const Vacancy_Ad_Request_Controller = require('../../controllers/VacancyAdRequestController')
-const partner_controller = require('../../controllers/partnerController')
-const location_controller = require('../../controllers/locationControllers')
-const message_controller = require('../../controllers/messageController')
+
+
 
 
 //show all the good admins
@@ -49,7 +47,7 @@ router.post('/create', async (req,res) => {
     }  
  })
  
-//delete an admin
+//delete
  router.delete('/:id', async (req,res) => {
     try {
      const id = req.params.id
@@ -61,53 +59,10 @@ router.post('/create', async (req,res) => {
         console.log(error)
     }  
  })
- //partner control
-
- //view all partners
-router.get('/viewAllPartners',partner_controller.getAllPartners)
-//Find a Partner by ID
-router.get('/findPartnerById',partner_controller.findPartner)
-// Delete a Partner
-router.delete('/deletePartner',partner_controller.deletePartner)
-//update a Partner
-router.put('/updatePartner',partner_controller.updatePartner)
 
 
-//partner location
 
- //Location controls
-  //view all location
-  router.get('/viewAllPartners',location_controller.getAllLocations)
-  //Find a location by ID
-  router.get('/findPartnerById',location_controller.findLocation)
-  // Delete a location
-  router.delete('/deletePartner',location_controller.deleteLocation)
-  // Update a location
-  router.put('/deletePartner',location_controller.updateLocation)
-
-//messaging
-
- //view all location
- router.put('/createMessage',message_controller.createMessage)
- //Find a message by ID
- router.get('/findMessageById',message_controller.findMessage)
- // Delete a Message
- router.delete('/deleteMessage',message_controller.deleteMessage)
  
-
-
-
-
-//vacany ad Requst controller
-
-  //View all request controller
-  router.get('/viewAllVacancyAdRequest', Vacancy_Ad_Request_Controller.get )
-  // Accept a vacancy ad request 
-  router.put('/acceptVacancyAdRequest', Vacancy_Ad_Request_Controller.update)
-  // Delete a Request
-  router.delete('/deleteVacancyAdRequest',Vacancy_Ad_Request_Controller.delete)
-  //creating a vacancy ad request
-  router.put('/createVacancyAd',Vacancy_Ad_Request_Controller.create)
  
  
  
