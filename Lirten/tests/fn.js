3f5d3d623d85c9c75a4ccf942fb17061b78afb3f
msharaf315 <msharaf315@gmail.com>
2019-04-03 19:59:47 +0300

    sprint 3 start kappa
    fixed some problems with testing changed the admin router and created a new file for admin tests

---
+++ Lirten/tests/fn.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const functions={
+viewAllAdmins: async() =>{
+const admins =  await axios.get('http://localhost:3000/api/admins/')
+//console.log(admins.data.data)
+return admins
+},
+viewAdminById:async(id)=>{
+    const admins =  await axios.get('http://localhost:3000/api/admins/?id='+id)
+    //console.log(admins.data.data)
+    return admins
+    
+},
+updateAdminById: async(id,body) =>{
+   const admin = await axios.put('http://localhost:3000/api/admins/?id='+id)
+   console.log(admin.data.data)
+   return admin
+}
+
+
+};
+module.exports = functions;
