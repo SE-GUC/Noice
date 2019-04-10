1df69a2c57b33fa3ed5dc56eba71721c1d0e11c0
adooma123 <adam.sadek@student.guc.edu.eg>
2019-04-10 20:24:59 +0200

    new file layout

---
+++ models/Location.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const LocationSchema = new Schema({
+    NameOfPlace: {
+        type: String,
+        required: true
+    },
+    ownerName: {
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        required: true
+    },
+    workingPlaceDepartments: [
+        {
+            nameOfDepartments: String,
+            City: String,
+            Region: String,
+            startTime: String,
+            endTime: String,
+            rate: Number,
+            isDepartmentAvailable: [{
+                nameOfSubdepartment: String,
+                isAvailable: Boolean,
+                capacity: Number,
+            }]
+        }
+    ]
+   
+})
+
+module.exports = User = mongoose.model('Location', LocationSchema)
