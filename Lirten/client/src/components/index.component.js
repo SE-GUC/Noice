331f08c1816c81a95c5eed976eb63ac17816c878
mohammedrefaat98 <mohrefaat98@gmail.com>
2019-04-13 00:22:21 +0200

    Update index.component.js

--- Lirten/client/src/components/index.component.js
+++ Lirten/client/src/components/index.component.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
 
-export default class Index extends Component {
-    render() {
-        return (
-            <div>
-                <p>Welcome to Index Component!!</p>
-            </div>
-        )
+export default class Index extends Component {constructor(props){
+    
+    super(props)
+    this.state={
+      partners:[]
     }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/events')
+     .then(res => this.setState({partners:res.data.data})
+     )
+  }
+  render() {
+    const partners = this.state.partners;
+    return this.state.partners.map( (partner) => {
+      return (
+        <ul>
+          id: {partner._id} |
+          age: {partner.Name} |
+          name: {partner.Owner} |
+          Company Name: {partner.Type}
+
+        </ul>
+      )
+    })
+  }
 }
