8d5c5748ed5ae8758d0e5c1c39d0e96fb7d43d16
msharaf315 <msharaf315@gmail.com>
2019-04-14 20:08:10 +0300

    front end update

--- client/src/store.js
+++ client/src/store.js
@@ -13,7 +13,9 @@ const store = createStore(
     rootReducer,
     intialState,
     compose(
-    applyMiddleware(...middleware)
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__&& 
+    window.__REDUX_DEVTOOLS_EXTENSION__()
     )
     );
 
