7c3ff393d9ee1a92d7a53b2a98abbfda9ff88439
kareemelshafey <kareemkimo39@gmail.com>
2019-04-14 20:40:26 +0200

    sharaf code integrated with the latest backend

---
+++ client/src/components/Home.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+//defining smth like the scheme
+import PropTypes from 'prop-types'
+//connects the components with the store
+import {connect} from 'react-redux';
+//import the actions that on your component
+import{getVacancies} from '../actions/homeActionsFolder/homeActions'
+
+
+class Home extends Component {
+
+//a life cycle method that runs once when you first run this component
+  componentWillMount(){
+  this.props.getVacancies();
+}
+//a must have life cycle method that runs each time you render a component
+  render() {  
+    const vacancyItem =  this.props.vacancies.map(vacancy =>(
+      <div Key={vacancy.id}>
+      <h3>{vacancy.careerLevel}</h3>
+      <p> {vacancy.description}</p>
+      </div>
+    ))
+    return (
+      <div>
+        <h1>Welcome Home</h1>
+        <h2>our latest Vacancies</h2>
+        {vacancyItem}
+      </div>
+    )
+  }
+}
+//defining the prop types that are passed to this component
+Home.propTypes={
+  getVacancies : PropTypes.func.isRequired,
+  home: PropTypes.array.isRequired
+  }
+
+
+const mapStateToProps = state =>({
+  home: state.home.items
+})
+
+export default connect(mapStateToProps, {getVacancies})(Home)
