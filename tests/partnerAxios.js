b884a3ecf8e6e15ad82f2aa8aaa313a39b1ee2a3
Omar-Hesham-Mohamed <omar.ibrahimyoussef@student.guc.edu.eg>
2019-04-17 00:19:30 +0200

    updates

--- tests/partnerAxios.js
+++ tests/partnerAxios.js
@@ -4,27 +4,27 @@ const functions = {
 
 
 	viewPartner: async () => {
-                const books = await axios.get('http://localhost:3000/api/users/partners/')
+                const books = await axios.get('http://localhost:5000/api/users/partners/')
                 return books
                 },
 
         viewPartnerid: async (id) => {
-                const books = await axios.get(`http://localhost:3000/api/users/partners/${id}`)
+                const books = await axios.get(`http://localhost:5000/api/users/partners/${id}`)
                 return books
                 },
 
         deletePartner: async (id) => {
-                const books = await axios.delete(`http://localhost:3000/api/users/partners/${id}`)
+                const books = await axios.delete(`http://localhost:5000/api/users/partners/${id}`)
                 return books
                 },
         
         updatePartner: async (id,req) => {
-                const books = await axios.put(`http://localhost:3000/api/users/partners/${id}`,req)
+                const books = await axios.put(`http://localhost:5000/api/users/partners/${id}`,req)
                 return books
                 },
 
         createpartner:async (req) => {
-                const partners = await axios.post("http://localhost:3000/api/users/partners",req)
+                const partners = await axios.post("http://localhost:5000/api/users/partners",req)
                 return partners
                 }
                 
