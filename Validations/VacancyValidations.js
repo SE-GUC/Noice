d964be4cfa865de309ba6e19ee21aeef98296fad
Omar-Hesham-Mohamed <omar.ibrahimyoussef@student.guc.edu.eg>
2019-04-17 13:41:57 +0200

    vacacncy

--- Validations/VacancyValidations.js
+++ Validations/VacancyValidations.js
@@ -3,12 +3,16 @@ const Joi = require('joi')
 module.exports = {
     createValidation: request => {
         const createSchema = {
+            title: Joi.string().min(1).max(500).required(),
             careerLevel: Joi.string().min(3).max(500).required(),
             jobDescription: Joi.string().min(3).max(100).required(),
-            educationLevel: Joi.string().min(0).max(3000).required(),
-            skillsRequired: Joi.string().min(0).max(3000).required(),
-            partnerId: Joi.string().min(0).max(25),
+            educationLevel: Joi.string().min(0).max(3000),
+            partnerId: Joi.string().min(0).max(3000),
+            time: Joi.string(),
+            skillsRequired: Joi.string().min(0).max(3000),
             applicants: Joi.array(),
+            status: Joi.boolean(),
+            closed: Joi.boolean(),
             tags: Joi.array()
 
         }
@@ -18,14 +22,26 @@ module.exports = {
 
     updateValidation: request => {
         const updateSchema = {
-            careerLevel: Joi.string().min(3).max(500),
-            jobDescription: Joi.string().min(3).max(100),
+            title: Joi.string().min(1).max(500).required(),
+            careerLevel: Joi.string().min(3).max(500).required(),
+            jobDescription: Joi.string().min(3).max(100).required(),
             educationLevel: Joi.string().min(0).max(3000),
+            partnerId: Joi.string().min(0).max(3000),
+            time: Joi.string(),
             skillsRequired: Joi.string().min(0).max(3000),
             applicants: Joi.array(),
-            tags: Joi.array(),
+            status: Joi.boolean(),
+            closed: Joi.boolean(),
+            tags: Joi.array()
         }
 
         return Joi.validate(request, updateSchema)
-    }, 
+    }, applyValidation: request => {
+        const applySchema = {
+            id: Joi.string().min(3).max(30),
+            
+        }
+
+        return Joi.validate(request, applySchema)
+    }
 }
