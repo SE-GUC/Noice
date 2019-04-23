f854e6ddb864fd2cf1ddb1c27bd1e6614902cdee
ANDREW-21-1 <andrew.ashraf@live.com>
2019-04-23 02:26:36 +0200

    soft Sign up

--- client/src/components/usersCruds/signUpForm.js
+++ client/src/components/usersCruds/signUpForm.js
@@ -1,5 +1,4 @@
 // importing react and connect
-import Home from '../../components/Home'
 import React, { Component } from 'react';
 //open a connection with the store
 import {connect} from 'react-redux';
@@ -7,16 +6,16 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 //import actions on this component
 import {signUp} from '../../actions/usersActionsFolder/usersActions';
-import {Switch,Route} from 'react-router-dom'
+
 //import bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-//requiring axios
-const axios = require('axios');
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
+
 
 
 
@@ -93,16 +92,7 @@ class signUpForm extends Component {
    }
    //send an axios request
    this.props.signUp(body)
-   
-   //reset the inputs to empty 
-   this.setState({
-     email:' ',
-     password:' ',
-     firstName:' ',
-     middleName:' ',
-     lastName:' ',
-     
-    })
+   this.routeChange()
   }
   
   //the life cycle method you need
@@ -145,24 +135,29 @@ class signUpForm extends Component {
       <Form.Control placeholder ="last name" type ="text" name="lastName" onChange = {this.onChange} value={this.state.lastName}/>
     </Form.Group>
     </Col>
-  </Form.Row>
-  <Form.Row>
-  <Col> 
-  <DropdownButton id="dropdown-item-button" title="Type of User">
-  <Dropdown.Item  onClick={this._onButtonClick}  as="button">Member</Dropdown.Item>
-  <Dropdown.Item onClick={this._onButtonClick1} as="button">Partner</Dropdown.Item>
-  <Dropdown.Item onClick={this._onButtonClick2} as="button">Co-working Space Owner</Dropdown.Item>
-</DropdownButton>
-</Col> 
+    
 </Form.Row>
 <Form.Row>
-<Form.Label></Form.Label>
+<Col>
+<Form.Group as={Col} >
+<Form.Label>Type of User</Form.Label>
+<ButtonToolbar>
+    <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+      <ToggleButton value={1} onClick={this._onButtonClick}>Member</ToggleButton>
+      <ToggleButton value={2} onClick={this._onButtonClick1}>Partner</ToggleButton>
+      <ToggleButton value={3} onClick={this._onButtonClick2}>Co-working Space Owner</ToggleButton>
+    </ToggleButtonGroup>
+  </ButtonToolbar>
+  </Form.Group>
+  </Col>
 </Form.Row>
 <Form.Row>
  <Col> 
-  <Button variant="primary" type="submit" onClick={this.onSubmit} >
+ <Form.Group as={Col} >
+  <Button variant="primary" type="submit" onClick={this.onSubmit}  >
     Create Account
   </Button>
+  </Form.Group>
   </Col> 
   </Form.Row>
 </Form>
